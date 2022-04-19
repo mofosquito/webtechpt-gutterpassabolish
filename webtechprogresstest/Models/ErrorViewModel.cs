01d34e9d1fdcf8ac55a50771c0e88da06bd21650
adrian <esjole@protonmail.com>
2022-04-19 14:56:20 +0800

    Add project files.

---
+++ webtechprogresstest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace webtechprogresstest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ webtechprogresstest/wwwroot/js/script.js
@@ -0,0 +1,4 @@
+﻿var namekey = document.getElementsByName("username")[0].value;
+if (namekey.length == 0) {
+    documnet.getElementByTagName('label')[0].style.color = "red";
+}
